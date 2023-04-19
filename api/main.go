80e0015f7fc3f58b00d8ed093212a25c20705c61
jd2rogers2 <jd2rogers2@gmail.com>
2023-04-19 08:29:34 -0700

    naming

--- api/main.go
+++ api/main.go
@@ -1,13 +1,15 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+	server := gin.Default()
+	server.GET("/ping", func(context *gin.Context) {
+		context.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	server.Run(":8080")
 }
